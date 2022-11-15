3cd9032ae723f41aa347816027ea7c08dc258c11
Jesus Cañedo <72284451+EidenKamisNet@users.noreply.github.com>
2022-11-15 15:59:48 -0600

    Add project files.

---
+++ LuxoftPOS/EFCashMastersDbContext.cs
@@ -0,0 +1,22 @@
+﻿using LuxoftPOS.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuxoftPOS
+{
+    public class EFCashMastersDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=EIDENKAMIS-PC\SQLEXPRESS2019;Initial catalog=CashMasters;Integrated Security=true");
+            //"Data Source=EIDENKAMIS-PC;Initial catalog=CashMasters;Integrated Security=true"
+            //Server =EIDENKAMIS-PC\SQLEXPRESS2019;Database=CashMasters;User Id=sa;Password=EidenKamis18235;
+            // base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ LuxoftPOS/Model/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuxoftPOS.Model
+{
+    public class Product
+    {
+        
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public double Price { get; set; }
+    }
+}
